83d3fc2273f73aa374a9898f3a816d9c14f9a28a
jeffureta <jeffvincentureta@gmail.com>
2023-08-02 00:08:55 +0100

    modified hello.py
    removed 'world' from the output and capitalize the first letter of the first name and last name

--- hello.py
+++ hello.py
@@ -4,6 +4,9 @@ name = input('Please enter your name: ')
 # Strip any whitespace from the name
 name = name.strip()
 
+# Capitalize the first letter of the first and last name
+name = name.title()
+
 # Print out hello and name
 
 # Will concat the string
@@ -13,4 +16,4 @@ name = name.strip()
 # print('Hello', name, sep=', ')
 
 # Will print out the string using format strings
-print(f'Hello World, {name}!')
+print(f'Hello, {name}!')
