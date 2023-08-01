1924c7f4d0049c64bda699ef549bc9b639540ea6
jeffureta <jeffvincentureta@gmail.com>
2023-08-02 00:01:43 +0100

    modified hello.py

--- hello.py
+++ hello.py
@@ -1,5 +1,12 @@
 # Ask user for name
 name = input('Please enter your name: ')
 # Print out hello and name
-print('Hello, ' + name)
-#print(f'Hello World, {name}!')
+
+# Will concat the string
+# print('Hello, ' + name)
+
+# Will print out the string using multiple arguments
+# print('Hello', name, sep=', ')
+
+# Will print out the string using format strings
+print(f'Hello World, {name}!')
