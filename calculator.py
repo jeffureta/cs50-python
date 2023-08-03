49fe048688cc45e595293b24f6492a5f61b77309
jeffureta <jeffvincentureta@gmail.com>
2023-08-03 01:55:52 +0100

    initial commit

--- calculator.py
+++ calculator.py
@@ -1,7 +1,7 @@
 # get two numbers from the user
-# and convert them to integers
-x = int(input('Enter a number: '))
-y = int(input('Enter another number: '))
+# and convert them to floats 
+x = float(input('Enter a number: '))
+y = float(input('Enter another number: '))
 
 # add the two numbers together
 print(f'{x} + {y} = {z}')
