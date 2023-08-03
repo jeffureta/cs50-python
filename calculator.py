97a8615467bb0106c37b71e999c94e4218eda31d
jeffureta <jeffvincentureta@gmail.com>
2023-08-03 01:47:59 +0100

    refactored calculator.py

--- calculator.py
+++ calculator.py
@@ -1,6 +1,6 @@
-x = input('Enter a number: ')
-y = input('Enter another number: ')
+# get two numbers from the user
+x = int(input('Enter a number: '))
+y = int(input('Enter another number: '))
 
-z = int(x) + int(y)
-
+# add the two numbers together
 print(f'{x} + {y} = {z}')
