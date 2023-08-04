6744cd7efdfc70ae6fe3521af5197c48a7c521e7
jeffureta <jeffvincentureta@gmail.com>
2023-08-04 23:05:26 +0100

    added square.py

---
+++ square.py
@@ -0,0 +1,9 @@
+def main():
+    x = int(input('What is X?: '))
+    print(f'X squared is {square(x)}')
+
+# define a function called square that takes in a number and returns the square of that number
+def square(n):
+    return n * n
+
+main()
