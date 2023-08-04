7629d800fd767c576b91e2bd20decf21c92c40a4
jeffureta <jeffvincentureta@gmail.com>
2023-08-04 01:28:57 +0100

    modified hello.py
    added a main function

--- hello.py
+++ hello.py
@@ -1,11 +1,16 @@
+def main():
+
+    # prompt the user for their name    
+    name = input('Enter your name: ')
+
+    # call the hello function
+    hello(name)
+
 def hello(name):
     '''
     Prints out a greeting to the user
     '''
     print(f"Hello, {name}!")
 
-# prompt the user for their name    
-name = input('Enter your name: ')
-
-# call the hello function
-hello(name)
+# call the main function
+main()
