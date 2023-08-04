cf9d8c48373b83e51f03e75f6db16de05c47e6e2
jeffureta <jeffvincentureta@gmail.com>
2023-08-04 01:58:17 +0100

    modified hello.py
    hello function will return 'Hello, World' if name == ''

--- hello.py
+++ hello.py
@@ -1,16 +1,19 @@
 def main():
-
     # prompt the user for their name    
     name = input('Enter your name: ')
 
     # call the hello function
     hello(name)
 
-def hello(name):
+def hello(to='World'):
     '''
-    Prints out a greeting to the user
+    Prints out a greeting to the user.
+    if no name is provided, or the name is an empty string, it will print hello, World!
     '''
-    print(f"Hello, {name}!")
+    if to == '':
+        to = 'World'
+    print(f"Hello, {to}!")
 
 # call the main function
 main()
+
